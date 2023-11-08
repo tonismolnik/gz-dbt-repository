5722ae308fb8215b129450ef034c0d43a1a5febd
tnsmlnk94@gmail.com <tnsmlnk94@gmail.com>
2023-11-08 15:09:59 +0000

    commit

--- models/intermediate/int_sales_margin.sql
+++ models/intermediate/int_sales_margin.sql
@@ -1,28 +1,12 @@
-WITH sales_data AS (
-
-    select
-        orders_id
-        , products_id
-        , revenue
-        , quantity
-    FROM {{ref('int_sales_margin')}}
-),
-
-product_data AS (
-    SELECT
-        products_id
-        , purchse_price
-    FROM {{ref('stg_raw__product')}}
-)
-
 SELECT
-    s.orders_id
+    s.date_date
+    , s.orders_id
     , s.products_id
     , s.revenue
     , s.quantity
-    , CAST(p.purchse_price AS FLOAT64) AS purchase_price
-    , ROUND (CAST(purchse_price AS FLOAT64)*quantity, 2) AS purchase_cost
-    , ROUND(revenue-ROUND(CAST(purchse_price AS FLOAT64)*quantity, 2), 2) AS margin
+    , p.purchase_price
+    , ROUND (p.purchase_price *s.quantity, 2) AS purchase_cost
+    , ROUND(revenue-purchase_price*quantity, 2) AS margin
 
 FROM {{ ref('stg_raw__sales') }} s
 LEFT JOIN {{ref ('stg_raw__product')}} p
